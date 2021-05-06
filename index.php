a45ae7e93e5db6f27beade9616328b3bb973f3c0
junaclaire <80485221+junaclaire@users.noreply.github.com>
2021-05-07 02:48:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,23 @@
-<?php 
-// John Longworth December 2016 
-$db = mysqli_connect('localhost', 'root', '', 'school_portal');
+<form action="action_page.php" method="post">
+  <div class="imgcontainer">
+    <img src="img_avatar2.png" alt="Avatar" class="avatar">
+  </div>
 
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
 
-if(isset($_GET['last'])){
-	$id = $_GET['last'];
-	$sql="SELECT * FROM students Where id=$id";//WE ARE TRYING TO GET THE NAME OF THE STUDENT BY ENTERING THE RANK
-$result=mysqli_query($db,$sql);
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {    
-    	
-      echo  $row["phone"];
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
 
-}
-}
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
 
-}
- /*if(isset($_GET['number1']) && isset($_GET['number2']) && isset($_GET['number3'])){
-	$number1 = $_GET['number1'];
-	$number2 = $_GET['number2'];
-	$number3 = $_GET['number3'];
-
-	mysqli_query($db, "INSERT INTO users (number1,number2,number3) VALUES('$number1','$number2','$number3')");
-	$_SESSION['message'] = "Info Saved!"; 
-	//header('location: setup.php');
-
-}*/
- ?>
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
